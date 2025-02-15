5ec38b452d638b36455d913b575c01f2854bbcce
Jay McCauley <jay.a.mccauley@gmail.com>
2025-02-15 15:59:02 -0500

    chore: update drizzle configuration for production

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,19 @@
 import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+// Load the appropriate .env file based on the environment
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else {
+  dotenv.config({ path: '.env.local' });
+}
 
 export default {
   schema: './db/schema.ts',
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || '',
+    connectionString: process.env.DATABASE_URL!,
     ssl: true,
   },
-} as Config; 
+} satisfies Config; 
